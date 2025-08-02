cb2048a35a0b14e4bcd6d6a6672f667b8edea5b3
rohitsingh906 <rohitkumarsingh906054@gmail.com>
2025-08-03 00:09:47 +0530

    Add files via upload

---
+++ first.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE COLLEGE ;
+USE COLLEGE ;
+CREATE TABLE student (
+rollno INT PRIMARY KEY ,
+name  VARCHAR(50) );
+
+insert into student 
+values
+(1 ," rohit ");
+select * from student ;
